c8ed0322ed5b966f0dcbd9c45c9d3370f8daf7cf
NicoMH <nmateluna16@alumnos.utalca.cl>
2020-12-09 21:13:34 -0300

    Modificación Código Inventario

--- inventario/__manifest__.py
+++ inventario/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "inventario",
+    'name': "Inventario",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Módulo Desarrollado para el ramo de Desarrollo de Aplicaciones Web II""",
 
     'description': """
-        Long description of module's purpose
+        Módulo creado para registrar las existencias en bodega
     """,
 
-    'author': "My Company",
+    'author': "NicolásMH",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Insurance',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,12 +23,12 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/views_inventario.xml',
+        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+#     'demo': [
+#         'demo/demo.xml',
+#     ],
 }
