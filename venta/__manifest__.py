984263589008dd1dca15be71170bbf09c6996270
sbrj-web <serojas16@alumnos.utalca.cl>
2020-12-12 15:49:14 -0300

    Tratando de subir todo

--- venta/__manifest__.py
+++ venta/__manifest__.py
@@ -3,20 +3,19 @@
     'name': "venta",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Módulo de ventas desarrollado para el proyecto de WEB2""",
 
     'description': """
-        Long description of module's purpose
+        Permite visualizar todas las salidas de un negocio tipo minimarket
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Sebastián",
+    'website': "www.utalca.cl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Insurance',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,12 +23,13 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/views_boleta.xml',
+       
+        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    # 'demo/demo.xml',
+    # ],
 }
