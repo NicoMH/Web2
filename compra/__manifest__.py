1be4f6386ec7dfe792b89b93588a4546d8d4d4ed
Davidmoravillar <dmora16@alumnos.utalca.cl>
2020-12-12 16:57:39 -0300

    Modificacion modulo compra

--- compra/__manifest__.py
+++ compra/__manifest__.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "compra",
+    'name': "compras",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Desarrollo del proyecto de Desarrollo de aplicacines web 2, modulo compras en Odoo""",
 
     'description': """
-        Long description of module's purpose
-    """,
+        Registrar los datos de los proveedores y las compras que el minimarket les realiza para abastecerse""",
 
-    'author': "My Company",
+    'author': "David Mora",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Insurance',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,12 +22,13 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/views_compras.xml',
+        'views/views_proveedor.xml',
+        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    #'demo': [
+        #'demo/demo.xml',
+    #],
 }
