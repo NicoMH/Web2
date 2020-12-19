931327be2564596f6ec863f4560b14a12847ee96
Davidmoravillar <dmora16@alumnos.utalca.cl>
2020-12-19 18:39:21 -0300

    Cambios en models y views de compra

--- compra/models/compras.py
+++ compra/models/compras.py
@@ -4,26 +4,26 @@ from odoo import models, fields, api
 
 class Compra(models.Model):
     _name='compra.compras'
-    _rec_name= 'proveedor_id'
+    _rec_name= 'nro_factura'
     fecha_compra=fields.Date( string = u'Fecha de emisión', default = fields.Date.context_today)
     observacion=fields.Text(blank=True, null=True)
     nro_factura=fields.Char(max_length=100)
     sub_total=fields.Integer()
     descuento=fields.Integer(default=0)
     total=fields.Integer()
-    proveedor_id=fields.Many2one('compra.proveedor', string = "Id del proveedor")
+    proveedor_id=fields.Many2one('compra.proveedor', string = " Proveedor")
     detalle_ids=fields.One2many('compra.detalle', 'compras_id', string = "Detalle de la compra")
+
 
 class DetalleCompra(models.Model):
     _name='compra.detalle'
-    _rec_name ='compras_id'
     producto=fields.Many2one('inventario.producto', string = "Producto")
     cantidad= fields.Integer()
     precio_compra=fields.Integer()
     sub_total=fields.Integer(string= 'Sub total', compute= '_sub_total')
     descuento=fields.Integer(default=0)
     total=fields.Integer(string= 'Total de la compra', compute= '_total_detalle')
-    compras_id=fields.Many2one('compra.compras', string = "Id de la compra")
+    compras_id=fields.Many2one('compra.compras', string = "Nro de Factura")
     @api.one
     def _sub_total(self):
         self.sub_total = (self.cantidad * self.precio_compra)
@@ -35,7 +35,7 @@ class DetalleCompra(models.Model):
 
 class Proveedor(models.Model):
     _name = 'compra.proveedor'
-
+    _rec_name='nombre'
     nombre=fields.Text(blank=True, null=True)
     direccion=fields.Text(blank=True, null=True)
     contacto=fields.Text(blank=True, null=True)
